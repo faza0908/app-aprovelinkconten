de63469b4cc850d1d19c191762665725a650b592
Fazalthaf <fazanaufalrafif@gmail.com>
2026-08-31 14:14:18 +0700

    update

--- app.py
+++ app.py
@@ -542,7 +542,7 @@ def main():
     with st.sidebar:
         col1, col2 = st.columns([1, 5])
     with col1:
-        st.image("images.png", width=40)
+        st.image("images.png", width=90)
     with col2:
         st.title("Aplikasi Persetujuan Konten")
     
